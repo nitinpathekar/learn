557420c3dcad95e5ff8f4ea662c45c066353ef6a
Nitin <nitinsardarpathekar@gmail.com>
2025-09-20 10:23:46 +0530

    this is 36 but in 35

--- 35/Main.java
+++ 35/Main.java
@@ -1,16 +1,37 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
         int n=30;
         // String bkyud=(String);
         // System.out.print(b);
-        int[] marks={1,2,2,41562,45};
-        marks[0]=45;
-        marks[1]=78;
-        marks[2]=91;
+        // int[] marks={1,2,2,41562,45};
+        // marks[0]=45;
+        // marks[1]=78;
+        // marks[2]=91;
 
-        for(int i=0;i<5;i++){
+        // for(int i=0;i<5;i++){
 
-        System.out.println(marks[i]);
-        }
+        // System.out.println(marks[i]);
+        // }
+
+
+
+        // int numb=1;
+
+        // //
+        // System.out.println(++numb);
+        // // numb++;
+        // System.out.println(numb);
+        
+
+        Scanner s = new Scanner(System.in);
+
+        int i;
+        i=s.nextInt();
+        System.out.println(i);
+        int j=s.nextInt();
+        System.out.print(j);
+
     }
 }
